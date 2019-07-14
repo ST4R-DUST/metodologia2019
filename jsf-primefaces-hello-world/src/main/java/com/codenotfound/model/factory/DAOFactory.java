c6e4613be6f66fe9643465fe3756394a1ff78d25
ST4R-DUST <52722577+ST4R-DUST@users.noreply.github.com>
2019-07-14 16:03:12 -0400

    Funcionalidad Academicos

--- jsf-primefaces-hello-world/src/main/java/com/codenotfound/model/factory/DAOFactory.java
+++ jsf-primefaces-hello-world/src/main/java/com/codenotfound/model/factory/DAOFactory.java
@@ -5,11 +5,14 @@ package com.codenotfound.model.factory;
 
 public abstract class DAOFactory {
     public static final int STUDENT_REPOSITORY = 1;
+    public static final int ACADEMIC_REPOSITORY = 2;
 
     public static DAOFactory getDAOFactory(int tipo) {
         switch (tipo) {
 	        case STUDENT_REPOSITORY:
-	            return new StudentDaoFactoryImpl();            
+	            return new StudentDaoFactoryImpl();           
+	        case ACADEMIC_REPOSITORY:
+	            return new AcademicDaoFactoryImpl(); 
 	        default:
 	            return null;
         }
